171565dd3ff3b207cf9c0cdcb64d9014f3d5b8f7
Evan <109839359+Evan260@users.noreply.github.com>
2025-11-05 14:54:18 -0700

    Remove SQL migration scripts

--- db_migrations/add_notify_new_reports.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add notify_new_reports to admin_notification_settings
--- Date: 2025-01-XX
--- Description: Adds notify_new_reports column to allow admins to opt-in to report notifications
-
-ALTER TABLE admin_notification_settings
-ADD COLUMN IF NOT EXISTS notify_new_reports BOOLEAN DEFAULT 1 AFTER notify_new_comments;
-
--- This migration is safe to run multiple times
